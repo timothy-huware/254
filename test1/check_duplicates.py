6c9975259dfc2aba250a32e858993fafaf38bbcc
Timothy James Foster <timothy.james@huware.com>
2024-07-11 15:03:57 +0200

    add: test1

---
+++ test1/check_duplicates.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df_1 = pd.read_csv("pod1.csv")
+df_2 = pd.read_csv("pod2.csv")
+df_3 = pd.read_csv("pod3.csv")
+
+# join df_1 and df_2 on email, localID, and insertID using pd.join
+
+df = pd.merge(df_1, df_2, on=["email", "localID", "insertID"])
+
+# join df on email, localID, and insertID
+df = pd.merge(df, df_3, on=["email", "localID", "insertID"])
+
+# print df
+print(df)
